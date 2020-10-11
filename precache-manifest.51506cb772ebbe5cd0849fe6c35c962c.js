a9c0c42bff31d547685cdabcf3e3d3057fd18dc2
Justin Roose <justin.d.roose@gmail.com>
2020-10-11 14:03:08 -0400

    Updates

---
+++ precache-manifest.51506cb772ebbe5cd0849fe6c35c962c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6556d0267ad9552fd5a2c2ee1e97e94",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "39d445daed8fde83703d",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9190e006f7d5b2f59d5c",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39d445daed8fde83703d",
+    "url": "/react-portfolio/static/js/2.0d8caff3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.0d8caff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9190e006f7d5b2f59d5c",
+    "url": "/react-portfolio/static/js/main.fa50558b.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
